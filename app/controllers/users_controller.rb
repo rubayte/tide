28977c992d08d6aa69cddd72c0d4b669d1b4fdce
rubayte-nki <r.rahman@nki.nl>
2015-06-30 13:20:12 +0200

    initial commit with app structure

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  
+  def autheticateUser
+    
+    if params[:email] == "" or params[:email] == nil or params[:password] == "" or params[:password] == nil
+      redirect_to :controller => "app", :action => "index"
+      flash[:notice] = "all fields must be completed!"
+      flash[:color]= "invalid"
+      return        
+    end
+    
+    if params[:email] == "r.rahman@nki.nl" and params[:password] == "test1234"  
+      redirect_to :home
+      return
+    else
+      redirect_to :controller => "app", :action => "index"
+      return
+    end 
+  end
+  
+  def home
+    @downloads = []
+    @tabactive = "dashboard"
+    if (params[:tab] == nil or params[:tab] == "")
+      @tabactive = "dashboard"
+    else
+      @tabactive = params[:tab]
+    end
+  end
+  
+  def logout
+    session[:user] = nil
+    redirect_to :controller => "app", :action => "index"
+    return  
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tide::Application.config.secret_token = 'a401742d69de72a349eeacf6379890216fc32d59719fd0a64cfbad263b7f065615784c0b9af93c31236da847594d4c8449b24fb5d3bf71580db37263f76ac0ec'
