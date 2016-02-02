eae990ab2bc6454b45b650553441d55ad64d9c78
rubayte-nki <r.rahman@nki.nl>
2016-02-02 13:47:07 +0100

    deploy in test env for confirmation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap.min
 //= require jquery
 //= require jquery.easing.min
 //= require jquery.fittext
+//= require bootstrap.min
 //= require cbpAnimatedHeader
 //= require wow.min
 //= require creative
