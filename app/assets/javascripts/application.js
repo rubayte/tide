1b352905e25897cbbda3607f84988fd49fe831a6
rubayte-nki <r.rahman@nki.nl>
2016-09-06 12:32:42 +0200

    edigene logo updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap.min
 //= require jquery
+//= require bootstrap.min
 //= require jquery.easing.min
 //= require jquery.dataTables.min
 //= require dataTables.bootstrap.min
